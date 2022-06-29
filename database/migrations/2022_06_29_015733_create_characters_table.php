2d2625bae9fe8d786e675918e3f21c2870a51f4c
Kevin Rider <github@thebackwaters.com>
2022-06-28 20:20:55 -0600

    Add character migrations

---
+++ database/migrations/2022_06_29_015733_create_characters_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->unsignedBigInteger('homeWorldId');
+            $table->unsignedBigInteger('speciesId');
+            $table->foreign('homeWorldId')->references('id')->on('planets');
+            $table->foreign('speciesId')->references('id')->on('species');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+};
---
+++ database/migrations/2022_06_29_021246_create_character_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('character_movies', function (Blueprint $table) {
+            $table->unsignedBigInteger('character_id');
+            $table->unsignedBigInteger('movie_id');
+            $table->unique(['character_id', 'movie_id']);
+            $table->foreign('character_id')->references('id')->on('characters');
+            $table->foreign('movie_id')->references('id')->on('movies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('character_movies');
+    }
+};
