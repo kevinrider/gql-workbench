874634fe6317fc1e31f73124540cdbef0a9c44f7
Kevin Rider <github@thebackwaters.com>
2022-07-14 21:38:22 -0600

    Add belongstomany character relationships

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Character extends Model
 {
     use HasFactory;
+
+    public function friends(): BelongsToMany
+    {
+        return $this->belongsToMany(Character::class, 'character_friend', 'friend_id');
+    }
+
+    public function movies(): BelongsToMany
+    {
+        return $this->belongsToMany(Movie::class, 'character_movie', 'character_id', 'movie_id');
+    }
 }
