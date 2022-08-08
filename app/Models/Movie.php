070a0399b8b77f8c95dbdb41cf872383b1cb215b
Kevin Rider <github@thebackwaters.com>
2022-08-07 21:50:09 -0600

    Add characters to movies

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Movie extends Model
 {
     use HasFactory;
+
+    public function characters(): BelongsToMany
+    {
+        return $this->belongsToMany(Character::class, 'character_movie', 'movie_id', 'character_id');
+    }
 }
